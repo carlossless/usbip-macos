d53f6c78f9c64a8f0a827c2b48acf55879e7c905
Karolis Stasaitis <contact@carlossless.io>
2025-05-11 13:27:32 +0200

    flake and referencing my own fork of objc2/objc2-generated to addresse the missing/wrong definitions

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+          clippy = toolchain;
+        };
+
+        buildInputs = with pkgs; [
+          pkg-config
+          libusb1
+          binutils
+        ] ++
+        (lib.optionals (stdenv.hostPlatform.isLinux) [
+          udev
+        ]) ++
+        (lib.optionals (stdenv.hostPlatform.isDarwin) [
+          darwin.apple_sdk.frameworks.IOKit
+          darwin.apple_sdk.frameworks.AppKit
+          iconv
+        ]);
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        packages = {
+          # For `nix build` `nix run`, & `nix profile install`:
+          default = naersk'.buildPackage {
+            pname = "usbip-darwin";
+            version = "latest";
+
+            src = ./.;
+
+            doCheck = false; # integration tests from running since they require an attached specific device
+
+            inherit buildInputs;
+
+            meta = with pkgs.lib; {
+              description = "A USB/IP client for macOS";
+              homepage = "https://github.com/carlossless/usbip-darwin";
+              license = licenses.mit;
+              mainProgram = "usbip-darwin";
+              maintainers = with maintainers; [ carlossless ];
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+          nativeBuildInputs = with pkgs; [ rustup toolchain ];
+        };
+      }
+    );
+}
